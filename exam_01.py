36acb6c3ccace212e7d343065ebb1f1cb9eee113
li jung kim <kij1467@gmail.com>
2023-04-11 12:09:36 +0900

    day_02

--- exam_01.py
+++ exam_01.py
@@ -1,12 +1,12 @@
 import itertools
 from utils import benchmark
 
-DAN = range(2, 10000)
+DAN = range(2, 100000)
 SEQ = range(1, 10)
 
 # 매 반복마다 print 함수 호출
-# case_01 함수 실행 시간: 12.4774 초
-# case_01 함수 메모리 사용량: 0.0117 MiB
+# case_01 함수 실행 시간: 2.8958 초
+# case_01 함수 메모리 사용량: 0.0078 MiB
 @benchmark
 def case_01():
     for i in DAN:
@@ -14,21 +14,21 @@ def case_01():
             print(f"{i} * {j} = {i * j}")
 
 # list에 추가한 뒤 한번에 출력
-# case_02 함수 실행 시간: 5.5980 초
-# case_02 함수 메모리 사용량: 1.4688 MiB
+# case_02 함수 실행 시간: 0.5788 초
+# case_02 함수 메모리 사용량: 1.9844 MiB
 @benchmark
 def case_02():
     result = [f"{i} * {j} = {i * j}\n" for i in DAN for j in SEQ]
     print("".join(result))
 
 # generator 사용
-# case_03 함수 실행 시간: 5.7436 초
-# case_03 함수 메모리 사용량: 0.7656 MiB
+# case_03 함수 실행 시간: 0.6699 초
+# case_03 함수 메모리 사용량: 1.8633 MiB
 @benchmark
 def case_03():
     result = (f"{i} * {j} = {i * j}\n" for i in DAN for j in SEQ)
     print("".join(result))
 
-case_01()
-case_02()
-case_03()
+# case_01()
+# case_02()
+# case_03()
